982d7de83d9287114f5f69bd1414f6009de06997
NP <K140705@gmail.com>
2016-02-10 04:34:44 +0000

    Tried to fix code by rewriting it, can't deploy on heroku yet

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,13 +1,12 @@
 class ContactMailer < ActionMailer::Base
   
-  default to: "K140705@gmail.com"
+  default to: 'k140705@gmail.com'
   
-  def contact_email (name, email, body)
-    
+  def contact_email(name, email, body)
     @name = name
     @email = email
     @body = body
     
-    mail (from: email, subject: "Contact Form Message")
+    mail(from: email, subject: 'Contact Form Message')
   end
 end
